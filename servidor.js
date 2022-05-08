2cde31c230fdb26c7b7f55d2319d764fd62d6144
ellpz04 <elopez.gc@gmail.com>
2022-05-08 09:15:13 -0500

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -5,7 +5,7 @@ http.createServer(function (request, respuesta) {
   respuesta.writeHead(200, {'Content-Type': 'text/plain'});
   respuesta.write("HolaS Mundo en Github");
   respuesta.end();
-}).listen(3000, 'localhost');
+}).listen(3000, 'https://ellpz04.github.io/node2o');
 
 
 console.log('Server running at https://ellpz04.github.io/node2o/);
